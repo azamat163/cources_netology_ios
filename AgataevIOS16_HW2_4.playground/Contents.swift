714fdb36ce99655860e78df93b551e893caf83aa
a.agataev <a.agataev@corp.mail.ru>
2021-07-26 09:20:12 +0300

    fix after review homework

--- AgataevIOS16_HW2_4.playground/Contents.swift
+++ AgataevIOS16_HW2_4.playground/Contents.swift
@@ -19,28 +19,45 @@ print("\(c ?? "default")")
 // Разница есть так как, сейчас у нас переменнас с не nil и следовательно в nil-coalescing будет выводится именна она, а впервом случае с = nil и принтился дефолт
 
 
-let oneString: String = "3d34234sdfs"
+let oneString: String = "3"
 let twoString: String = "20"
 let threeString: String = "12"
-let fourString: String = "ta"
+let fourString: String = "10"
 let fiveString: String = "30"
 
-var sum: Int = 0
-
 // optional binding
-
-
-
+var sum1: Int = 0
 if let temp1 = Int(oneString), let temp2 = Int(twoString), let temp3 = Int(threeString), let temp4 = Int(fourString), let temp5 = Int(fiveString) {
-    sum = temp1 + temp2 + temp3 + temp4 + temp5
-    print(sum)
+    sum1 = temp1 + temp2 + temp3 + temp4 + temp5
+    print(sum1)
 } else {
-   print(sum)
+   print(sum1)
 }
 
 // unwrap operator
-sum = Int(oneString)! + Int(twoString)! + Int(threeString)! + Int(fourString)! + Int(fiveString)!
-// возникнет ошибка, так как те константы, которые содержат в себе буквы при конвертации будут равны nil, а используя ! оператора мы как раз принудительно извлекаем значение
+var sum2: Int = 0
+if Int(oneString) != nil {
+    sum2 += Int(oneString)!
+}
+
+if Int(twoString) != nil {
+    sum2 += Int(twoString)!
+}
+
+if Int(threeString) != nil {
+    sum2 += Int(threeString)!
+}
+
+if Int(fourString) != nil {
+    sum2 += Int(fourString)!
+}
+
+if Int(fiveString) != nil {
+    sum2 += Int(fiveString)!
+}
+
+print(sum2)
+// c oператором ! нужно работать осторожно именно убедится, что переменная не nil и только в таком случае принудительно извлекать значение
 
 
 //Задача 2
