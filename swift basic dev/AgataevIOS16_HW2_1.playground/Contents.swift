7e52bc3204f77df14f0d29f9d0e28f13106d683e
a.agataev <a.agataev@corp.mail.ru>
2021-09-03 10:07:50 +0300

    fix task 2_1

--- swift basic dev/AgataevIOS16_HW2_1.playground/Contents.swift
+++ swift basic dev/AgataevIOS16_HW2_1.playground/Contents.swift
@@ -1,145 +1,171 @@
 import UIKit
 
-// Задача №1
-
-// Создали enum каналов
-enum TelevisionChannel {
-    case muztv, friday, russiatoday, rentv, tnt
-}
-
 // Базовый протокол(интерфейс), в котором описаны основные свойства и методы, в котором будут применятся в классе
 protocol BaseProtocol {
-    var model: String { get set }
+    var tvInfo: (String, String) { get set }
     var isTurnOn: Bool { get set }
-    var currentChannel: TelevisionChannel { get set }
     func showNowOnTv() -> String
-    
 }
 
-class TelevisionOne: BaseProtocol {
+// Задача №1
+
+class FirstTelevision: BaseProtocol {
+    // Создали enum каналов
+    enum TelevisionChannel {
+        case muztv, friday, russiatoday, rentv, tnt
+    }
+    
     // свойства класса
-    internal var model: String
-    internal var isTurnOn: Bool
-    internal var currentChannel: TelevisionChannel
+    var tvInfo: (String, String)
+    var isTurnOn: Bool
+    var currentChannel: TelevisionChannel
     
     // инициализатор класса
-    init(model: String, isTurnOn: Bool, currentChannel: TelevisionChannel) {
-        self.model = model
+    init(tvInfo: (String, String), isTurnOn: Bool, currentChannel: TelevisionChannel) {
+        self.tvInfo = tvInfo
         self.isTurnOn = isTurnOn
         self.currentChannel = currentChannel
     }
     
     // метод который показывает текущий канал
-    internal func showNowOnTv() -> String {
-        switch currentChannel {
-        case .muztv:
-            return "канал муз-тв"
-        case .friday:
-            return "канал пятница"
-        case .russiatoday:
-            return "канал Russia Today"
-        case .rentv:
-            return "канал Рен-тв"
-        case .tnt:
-            return "канал тнт"
+    func showNowOnTv() -> String {
+        if isTurnOn {
+            switch currentChannel {
+            case .muztv:
+                return "канал муз-тв"
+            case .friday:
+                return "канал пятница"
+            case .russiatoday:
+                return "канал Russia Today"
+            case .rentv:
+                return "канал Рен-тв"
+            case .tnt:
+                return "канал тнт"
+            }
+        } else {
+            return "телевизор \(tvInfo.0) выключен. Необходимо включить!"
         }
     }
 }
 
 
-let tv = TelevisionOne(model: "Samsung", isTurnOn: true, currentChannel: .muztv)
-print(tv.showNowOnTv())
+let firstTv = FirstTelevision(tvInfo: ("samsung", "ue50tu7170u"), isTurnOn: true, currentChannel: .muztv)
+print(firstTv.showNowOnTv())
 
-tv.model = "Xiami"
-tv.currentChannel = .russiatoday
-print(tv.showNowOnTv())
+firstTv.tvInfo = ("Xiami", "4S 43")
+firstTv.isTurnOn = false
+firstTv.currentChannel = .russiatoday
+print(firstTv.showNowOnTv())
 
 
 
 // Задача №2
-
 // структура Настройка
 struct Setting {
     let volume: Double
     let isColor: Bool
-}
-
-class TelevisionTwo: BaseProtocol {
-    // свойства класса
-    internal var model: String
-    internal var isTurnOn: Bool
-    internal var currentChannel: TelevisionChannel
-    internal var setting: Setting
     
-    // инициализатор класса
-    init(model: String, isTurnOn: Bool, currentChannel: TelevisionChannel, setting: Setting) {
-        self.model = model
-        self.isTurnOn = isTurnOn
-        self.currentChannel = currentChannel
-        self.setting = setting
+    // добавляем инициализатор с целью проверки на громкость
+    init(volume: Double, isColor: Bool) {
+        self.volume = Setting.getVolume(volume)
+        self.isColor = isColor
     }
-    
-    // метод который показывает текущий канал
-    internal func showNowOnTv() -> String {
-        switch currentChannel {
-        case .muztv:
-            return "канал муз-тв c громкостью \(self.setting.volume)"
-        case .friday:
-            return "канал пятница  c громкостью \(self.setting.volume)"
-        case .russiatoday:
-            return "канал Russia Today  c громкостью \(self.setting.volume)"
-        case .rentv:
-            return "канал Рен-тв  c громкостью \(self.setting.volume)"
-        case .tnt:
-            return "канал тнт  c громкостью \(self.setting.volume)"
+
+    // функция проверка громкости
+    static func getVolume(_ volume: Double) -> Double {
+        switch volume {
+        case let i where i < 0:
+            return 0
+        case let i where i > 1:
+            return 1
+        default:
+            return volume
         }
     }
 }
 
+class SecondTelevision: FirstTelevision {
+    // свойства класса
+    var setting: Setting
+    
+    // инициализатор класса
+    init(tvInfo: (String, String), isTurnOn: Bool, currentChannel: TelevisionChannel, setting: Setting) {
+        self.setting = setting
+        super.init(tvInfo: tvInfo, isTurnOn: isTurnOn, currentChannel: currentChannel)
+    }
+    
+    // метод который показывает текущий канал
+    override func showNowOnTv() -> String {
+        if isTurnOn {
+            switch currentChannel {
+            case .muztv:
+                return "телевизор по канал муз-тв c громкостью \(self.setting.volume)"
+            case .friday:
+                return "канал пятница  c громкостью \(self.setting.volume)"
+            case .russiatoday:
+                return "канал Russia Today  c громкостью \(self.setting.volume)"
+            case .rentv:
+                return "канал Рен-тв  c громкостью \(self.setting.volume)"
+            case .tnt:
+                return "канал тнт  c громкостью \(self.setting.volume)"
+            }
+        } else {
+            return "телевизор \(tvInfo.0) выключен. Необходимо включить!"
+        }
+    }
+}
 
 let setting = Setting(volume: 0.5, isColor: true)
-let tvTwo = TelevisionTwo(model: "Samsung", isTurnOn: true, currentChannel: .muztv, setting: setting)
-print(tvTwo.showNowOnTv())
+let secondTv = SecondTelevision(tvInfo: ("samsung", "ue50tu7170u"), isTurnOn: true, currentChannel: .muztv, setting: Setting(volume: -2, isColor: true))
+print(secondTv.showNowOnTv())
 
-tvTwo.model = "Xiami"
-tvTwo.currentChannel = .russiatoday
-print(tvTwo.showNowOnTv())
+secondTv.tvInfo = ("LG", "OLED55E9P 55")
+secondTv.setting = Setting(volume: 0.6, isColor: false)
+secondTv.currentChannel = .russiatoday
+print(secondTv.showNowOnTv())
 
 
 // Задача №3
-
-
-enum Channel {
-    case telechannel(String)
-    case port(String)
-}
-
-class TelevisionThree {
+class ThirdTelevision: BaseProtocol {
+    // енам входа просмотров
+    enum Input {
+        case channel(String)
+        case port(String)
+    }
+    
     // свойства класса
-    internal var model: String
-    internal var isTurnOn: Bool
-    internal var channel: Channel
+    var tvInfo: (String, String)
+    var isTurnOn: Bool
+    var input: Input
     
     // инициализатор класса
-    init(model: String, isTurnOn: Bool, channel: Channel) {
-        self.model = model
+    init(tvInfo: (String, String), isTurnOn: Bool, input: Input) {
+        self.tvInfo = tvInfo
         self.isTurnOn = isTurnOn
-        self.channel = channel
+        self.input = input
     }
     
     // метод который показывает текущий канал
-    internal func showNowOnTv() -> String {
-        switch channel {
-        case .telechannel(let telechannel):
-            return "канал \(telechannel)"
-        case .port(let port):
-            return "по порту \(port)"
+    func showNowOnTv() -> String {
+        if isTurnOn {
+            switch input {
+            case .channel(let telechannel):
+                return "канал \(telechannel)"
+            case .port(let port):
+                return "по порту \(port)"
+            }
+        } else {
+            return "телевизор \(tvInfo.0) выключен. Необходимо включить!"
         }
     }
 }
 
-let tvThree1 = TelevisionThree(model: "Samsung", isTurnOn: true, channel: Channel.telechannel("muz-tv"))
-print(tvThree1.showNowOnTv())
+let thirdTvChannelTurnOn = ThirdTelevision(tvInfo: ("LG", "OLED55E9P 55"), isTurnOn: true, input: .channel("muz-tv"))
+print(thirdTvChannelTurnOn.showNowOnTv())
+let thirdTvChannelTurnOff = ThirdTelevision(tvInfo: ("LG", "OLED55E9P 55"), isTurnOn: false, input: .channel("muz-tv"))
+print(thirdTvChannelTurnOff.showNowOnTv())
 
-let tvThree2 = TelevisionThree(model: "Samsung", isTurnOn: true, channel: Channel.port("COM"))
-print(tvThree2.showNowOnTv())
+let thirdTvPortTurnOn = ThirdTelevision(tvInfo: ("Samsung", "OLED55E9P 55"), isTurnOn: true, input: .port("COM"))
+print(thirdTvPortTurnOn.showNowOnTv())
+let thirdTvPortTurnOff = ThirdTelevision(tvInfo: ("Samsung", "OLED55E9P 55"), isTurnOn: false, input: .port("COM"))
+print(thirdTvPortTurnOff.showNowOnTv())
